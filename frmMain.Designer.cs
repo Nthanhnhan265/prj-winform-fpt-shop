5517529bc92ad13e8daa37308cab6511bbdfaa82
Nthanhnhan265 <ngthanhnhan265.xb@gmail.com>
2023-10-20 21:55:11 +0700

    changed name

--- frmMain.Designer.cs
+++ frmMain.Designer.cs
@@ -34,7 +34,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(330, 85);
+            this.label1.Location = new System.Drawing.Point(367, 31);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(51, 20);
             this.label1.TabIndex = 0;
