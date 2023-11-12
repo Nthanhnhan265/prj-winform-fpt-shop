844cb706cbc9b36fd718bd61e6d767747892c891
Nthanhnhan265 <ngthanhnhan265.xb@gmail.com>
2023-11-12 15:06:25 +0700

    before pull

--- db_fptshop.sql
+++ db_fptshop.sql
@@ -1,36 +1,39 @@
-﻿-- Tạo Database QuanLy
+﻿use master
+drop database QuanLyCuaHang
+-- Tạo Database QuanLy
 create database QuanLyCuaHang
-
+go
+use QuanLyCuaHang
 -- Tạo Nhân Viên
 create table NhanVien(
 MaNV char(10) not null,
-HoTen nvarchar(50),
+HoTen nvarchar(50) not null,
 NgaySinh date,
-DiaChi nvarchar(50),
-GioiTinh nvarchar(50),
-CCCD char(10),
-SDT char(10),
-Email nvarchar(50),
+DiaChi nvarchar(255) not null,
+GioiTinh NVARCHAR(5) CHECK (GioiTinh IN (N'Nam', N'Nữ')),
+CCCD char(10) not null,
+SDT char(10) not null,
+Email nvarchar(50) not null,
 MaCH char(10),
 QuanLy char(10),
-MatKhau char(10),
-QuyenHan nvarchar(50),
+MatKhau char(10) not null,
+QuyenHan nvarchar(50) CHECK (QuyenHan IN(N'quản lý',N'nhân viên')),
 primary key(MaNV))
 
 -- Tạo Nhà Sản Xuất
 create table NhaSX(
 MaNSX char(10) not null,
-TenNSX nvarchar(50),
+TenNSX nvarchar(50) not null ,
 primary key(MaNSX))
 
 -- Tạo Sản Phẩm
 create table SanPham(
 MaSP char(10) not null,
-TenSP nvarchar(50),
+TenSP nvarchar(50) not null,
 ThongTinSP Text,
 MaDM char(10),
 MaNSX char(10),
-GiaBan char(10),
+GiaBan int,
 Image nvarchar(50),
 primary key(MaSP))
 
@@ -39,7 +42,7 @@ create table ChiTietHD(
 MaChiTiet char(10) not null,
 MaHD char(10),
 MaSP char(10),
-SoLuong char(10),
+SoLuong int,
 primary key(MaChiTiet))
 
 -- Tạo Danh Mục
@@ -53,39 +56,46 @@ primary key(MaDM))
 create table CuaHang(
 MaCH char(10) not null,
 TenCH nvarchar(50),
-DiaChi nvarchar(50),
-SoDienThoai char(10),
-QuanLy nvarchar(50),
+DiaChi nvarchar(255) not null,
+SoDienThoai char(10) not null,
+QuanLy char(10) not null,
 NgayKhaiTruong datetime,
 primary key(MaCH))
 
 -- Tạo Hóa Đơn
 create table HoaDon(
-MaHD char(10) not null,
-MaKH char(10),
-MaNV char(10),
-NgayTao datetime,
-MaCH char(10),
-PTThanhToan nvarchar(50),
-SoTienTT char(10),
+	MaHD CHAR(10) NOT NULL,
+    MaKH CHAR(10) NOT NULL,
+    MaNV CHAR(10) NOT NULL,
+    NgayTao DATETIME NOT NULL,
+    MaCH CHAR(10) NOT NULL,
+    PTThanhToan NVARCHAR(50) NOT NULL,
+    SoTienTT INT NOT NULL,
 primary key(MaHD))
 
 -- Tạo Khách Hàng
-create table KhachHang(
-MaKH char(10) not null,
-HoTenKH nvarchar(50),
-CCCD char(10),
-SDT char(10),
-DiaChi nvarchar(50),
-Email nvarchar(50),
-primary key(MaKH))
+CREATE TABLE KhachHang (
+    MaKH CHAR(10) NOT NULL,
+    HoTenKH NVARCHAR(50) NOT NULL,
+    CCCD CHAR(10) NOT NULL,
+    SDT CHAR(10) NOT NULL,
+    DiaChi NVARCHAR(255) NOT NULL,
+    Email NVARCHAR(50),
+    PRIMARY KEY (MaKH)
+);
 
 -- Tạo Tồn Kho
 create table TonKho(
 MaCH char(10) not null,
 MaSP char(10) not null,
-SoLuong char(10),
+SoLuong int,
 primary key(MaCH,MaSP))
+go
+--Tạo liên kết của hàng - nhân viên
+alter table CuaHang 
+add constraint NV_CH_QuanLy
+foreign key(QuanLy)
+references NhanVien(MaNV)
 
 -- Tạo liên kết NhânViên - CửaHàng
 alter table NhanVien
@@ -117,11 +127,11 @@ add constraint SP_CTHD_MaSP
 foreign key(MaSP)
 references SanPham(MaSP)
 
--- Tạo lên kết sanpham - doanhmuc
+-- Tạo lên kết sanpham - danhmuc
 alter table SanPham
 add constraint DM_SP_MaDM
 foreign key(MaDM)
-references DoanhMuc(MaDM)
+references DanhMuc(MaDM)
 
 -- Tạo liên kết chitiethoandon - hoadon
 alter table ChiTietHD
@@ -147,3 +157,41 @@ add constraint CH_TK_MaCH
 foreign key(MaCH)
 references CuaHang(MaCH)
 
+-- Thêm ràng buộc NOT NULL cho trường GioiTinh trong bảng NhanVien
+ALTER TABLE NhanVien
+ALTER COLUMN GioiTinh NVARCHAR(50) NOT NULL;
+
+-- Thêm ràng buộc CHECK cho trường GioiTinh trong bảng NhanVien
+ALTER TABLE NhanVien
+ADD CONSTRAINT CK_GioiTinh
+CHECK (GioiTinh IN ('Nam', 'Nữ'));
+
+--ràng buộc CHECK cho phương thức thanh toán 
+ALTER TABLE HoaDon
+ADD CONSTRAINT CK_PPThanhToan
+CHECK (PTThanhToan IN ('Tiền mặt','Chuyển khoản'))
+
+-- Thêm ràng buộc CHECK cho trường SDT trong bảng NhanVien
+ALTER TABLE NhanVien
+ADD CONSTRAINT CK_SDT_NV
+CHECK (LEN(SDT) = 10);
+
+-- Thêm ràng buộc CHECK cho trường SDT trong bảng KhachHang
+ALTER TABLE KhachHang
+ADD CONSTRAINT CK_SDT_KH
+CHECK (LEN(SDT) = 10);
+
+-- Thêm ràng buộc CHECK cho trường SDT trong bảng CuaHang
+ALTER TABLE CuaHang
+ADD CONSTRAINT CK_SDT_CH
+CHECK (LEN(SoDienThoai) = 10);
+
+-- Thêm ràng buộc CHECK cho trường CCCD trong bảng NhanVien
+ALTER TABLE NhanVien
+ADD CONSTRAINT CK_CCCD_NV
+CHECK (LEN(CCCD) = 10);
+
+-- Thêm ràng buộc CHECK cho trường CCCD trong bảng KhachHang
+ALTER TABLE KhachHang
+ADD CONSTRAINT CK_CCCD_KH
+CHECK (LEN(CCCD) = 10);
